58cd1bfb80501ff61bd5938ef9543282606f3244
schwabmatthewl-glitch <schwab.matthewlee@gmail.com>
2026-02-14 21:14:23 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -14,3 +14,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register Service Worker for PWA support
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Wagachat SW registered!', reg))
+      .catch(err => console.log('Wagachat SW registration failed:', err));
+  });
+}
